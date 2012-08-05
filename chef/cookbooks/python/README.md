7c33b9fa4ce3ffc01716ec8fdb7970d14239271a
Andrew Loewenstern <drue@gigagig.org>
2012-08-05 00:42:33 -0700

    update python cookbook

--- chef/cookbooks/python/README.md
+++ chef/cookbooks/python/README.md
@@ -16,13 +16,18 @@ Cookbooks
 ---------
 
 * build-essential
+* yum
+
+NOTE: The `yum` cookbook is a dependency of the cookbook, and will be used to install [EPEL](http://fedoraproject.org/wiki/EPEL) on RedHet/CentOS 5.x systems to provide the Python 2.6 packages.
 
 Attributes
 ==========
 
-* `node["python"]["install_method"]` = method to install python with, default `package`.
+See `attributes/default.rb` for default values.
 
-The file also contains the following attribute types:
+* `node["python"]["install_method"]` - method to install python with, default `package`.
+
+The file also contains the following attributes:
 
 * platform specific locations and settings.
 * source installation settings
@@ -53,26 +58,27 @@ Install packages using the new hotness in Python package management...[`pip`](ht
 - version: the version of the package to install/upgrade.  If no version is given latest is assumed.
 - virtualenv: virtualenv environment to install pip package into
 - options: Add additional options to the underlying pip package command
+- timeout: timeout in seconds for the command to execute. Useful for pip packages that may take a long time to install. Default 900 seconds.
 
 # Example
-    
+
     # install latest gunicorn into system path
     python_pip "gunicorn" do
       action :install
     end
-    
+
     # target a virtualenv
     python_pip "gunicorn" do
       virtualenv "/home/ubunut/my_ve"
       action :install
     end
-    
+
     # install Django 1.1.4
     python_pip "django" do
       version "1.1.4"
       action :install
     end
-    
+
     # use this provider with the core package resource
     package "django" do
       provider Chef::Provider::PythonPip
@@ -95,16 +101,17 @@ Install packages using the new hotness in Python package management...[`pip`](ht
 - interpreter: The Python interpreter to use. default is `python2.6`
 - owner: The owner for the virtualenv
 - group: The group owner of the file (string or id)
+- options : Command line options (string)
 
 # Example
-    
+
     # create a 2.6 virtualenv owned by ubuntu user
     python_virtualenv "/home/ubuntu/my_cool_ve" do
       owner "ubuntu"
       group "ubuntu"
       action :create
     end
-    
+
     # create a Python 2.4 virtualenv
     python_virtualenv "/home/ubuntu/my_old_ve" do
       interpreter "python2.4"
@@ -112,7 +119,15 @@ Install packages using the new hotness in Python package management...[`pip`](ht
       group "ubuntu"
       action :create
     end
-    
+
+    # create a Python 2.6 virtualenv with access to the global packages owned by ubuntu user
+    python_virtualenv "/home/ubuntu/my_old_ve" do
+      owner "ubuntu"
+      group "ubuntu"
+      options "--system-site-packages"
+      action :create
+    end
+
 Usage
 =====
 
@@ -140,7 +155,6 @@ virtualenv
 ----------
 
 Installs virtualenv using the `python_pip` resource.
-
 
 License and Author
 ==================
--- chef/cookbooks/python/providers/pip.rb
+++ chef/cookbooks/python/providers/pip.rb
@@ -22,7 +22,7 @@ require 'chef/mixin/shell_out'
 require 'chef/mixin/language'
 include Chef::Mixin::ShellOut
 
-# the logic in all action methods mirror that of 
+# the logic in all action methods mirror that of
 # the Chef::Provider::Package which will make
 # refactoring into core chef easy
 
@@ -34,10 +34,16 @@ action :install do
   elsif @current_resource.version == nil
     install_version = candidate_version
   end
-  
+
+  # Set the timeout (units in seconds)
+  timeout = 900
+  if @new_resource.timeout
+    timeout = @new_resource.timeout
+  end
+
   if install_version
     Chef::Log.info("Installing #{@new_resource} version #{install_version}")
-    status = install_package(@new_resource.package_name, install_version)
+    status = install_package(@new_resource.package_name, install_version, timeout)
     if status
       @new_resource.updated_by_last_action(true)
     end
@@ -45,10 +51,16 @@ action :install do
 end
 
 action :upgrade do
+  # Set the timeout (units in seconds)
+  timeout = 900
+  if @new_resource.timeout
+    timeout = @new_resource.timeout
+  end
+
   if @current_resource.version != candidate_version
     orig_version = @current_resource.version || "uninstalled"
     Chef::Log.info("Upgrading #{@new_resource} version from #{orig_version} to #{candidate_version}")
-    status = upgrade_package(@new_resource.package_name, candidate_version)
+    status = upgrade_package(@new_resource.package_name, candidate_version, timeout)
     if status
       @new_resource.updated_by_last_action(true)
     end
@@ -56,9 +68,15 @@ action :upgrade do
 end
 
 action :remove do
+  # Set the timeout (units in seconds)
+  timeout = 900
+  if @new_resource.timeout
+    timeout = @new_resource.timeout
+  end
+
   if removing_package?
     Chef::Log.info("Removing #{@new_resource}")
-    remove_package(@current_resource.package_name, @new_resource.version)
+    remove_package(@current_resource.package_name, @new_resource.version, timeout)
     @new_resource.updated_by_last_action(true)
   else
   end
@@ -80,27 +98,27 @@ def expand_options(options)
   options ? " #{options}" : ""
 end
 
-# these methods are the required overrides of 
-# a provider that extends from Chef::Provider::Package 
+# these methods are the required overrides of
+# a provider that extends from Chef::Provider::Package
 # so refactoring into core Chef should be easy
 
 def load_current_resource
   @current_resource = Chef::Resource::PythonPip.new(@new_resource.name)
   @current_resource.package_name(@new_resource.package_name)
   @current_resource.version(nil)
-  
+
   unless current_installed_version.nil?
     @current_resource.version(current_installed_version)
   end
-  
+
   @current_resource
 end
 
 def current_installed_version
   @current_installed_version ||= begin
     delimeter = /==/
-    
-    version_check_cmd = "pip freeze#{expand_virtualenv(can_haz_virtualenv(@new_resource))} | grep -i #{@new_resource.package_name}=="
+
+    version_check_cmd = "#{pip_cmd(@new_resource)} freeze | grep -i '^#{@new_resource.package_name}=='"
     # incase you upgrade pip with pip!
     if @new_resource.package_name.eql?('pip')
       delimeter = /\s/
@@ -109,38 +127,41 @@ def current_installed_version
     p = shell_out!(version_check_cmd)
     p.stdout.split(delimeter)[1].strip
   rescue Chef::Exceptions::ShellCommandFailed
+  rescue Mixlib::ShellOut::ShellCommandFailed
   end
 end
 
 def candidate_version
   @candidate_version ||= begin
     # `pip search` doesn't return versions yet
-    # `pip list` may be coming soon: 
+    # `pip list` may be coming soon:
     # https://bitbucket.org/ianb/pip/issue/197/option-to-show-what-version-would-be
     @new_resource.version||'latest'
   end
 end
 
-def install_package(name,version)
+def install_package(name, version, timeout)
   v = "==#{version}" unless version.eql?('latest')
-  shell_out!("pip install#{expand_options(@new_resource.options)}#{expand_virtualenv(can_haz_virtualenv(@new_resource))} #{name}#{v}")
+  shell_out!("#{pip_cmd(@new_resource)} install#{expand_options(@new_resource.options)} #{name}#{v}", :timeout => timeout)
 end
 
-def upgrade_package(name, version)
+def upgrade_package(name, version, timeout)
   v = "==#{version}" unless version.eql?('latest')
-  shell_out!("pip install --upgrade#{expand_options(@new_resource.options)}#{expand_virtualenv(can_haz_virtualenv(@new_resource))} #{@new_resource.name}#{v}")
+  shell_out!("#{pip_cmd(@new_resource)} install --upgrade#{expand_options(@new_resource.options)} #{@new_resource.name}#{v}", :timeout => timeout)
 end
 
-def remove_package(name, version)
-  shell_out!("pip uninstall -y#{expand_options(@new_resource.options)}#{expand_virtualenv(can_haz_virtualenv(@new_resource))} #{@new_resource.name}")
-end
-
-def expand_virtualenv(virtualenv)
-  virtualenv && " --environment=#{virtualenv}"
+def remove_package(name, version, timeout)
+  shell_out!("#{pip_cmd(@new_resource)} uninstall -y#{expand_options(@new_resource.options)} #{@new_resource.name}", :timeout => timeout)
 end
 
 # TODO remove when provider is moved into Chef core
 # this allows PythonPip to work with Chef::Resource::Package
-def can_haz_virtualenv(nr)
-  nr.respond_to?("virtualenv") ? nr.virtualenv : nil
+def pip_cmd(nr)
+  if (nr.respond_to?("virtualenv") && nr.virtualenv)
+    ::File.join(nr.virtualenv,'/bin/pip')
+  elsif "#{node['python']['install_method']}".eql?("source")
+    ::File.join("#{node['python']['prefix_dir']}","/bin/pip")
+  else
+    'pip'
+  end
 end
