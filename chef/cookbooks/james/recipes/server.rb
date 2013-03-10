bd3d5e46c4748744a0f2ced26017fd6a826fbad6
Andrew Loewenstern <drue@gigagig.org>
2013-03-09 20:30:05 -0800

    server recipe

---
+++ chef/cookbooks/james/recipes/server.rb
@@ -0,0 +1,14 @@
+directory "/var/audio" do
+  mode "0644"
+end
+
+cookbook_file "/etc/init/james.conf" do
+  source "upstart.conf"
+end
+
+service "james" do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+  
