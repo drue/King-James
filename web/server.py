6abd93b7219b1503f7b756b18335709187daf1a6
Andrew Loewenstern <drue@gigagig.org>
2013-03-11 00:14:54 -0700

    reorg for script

--- web/server.py
+++ web/server.py
@@ -7,38 +7,42 @@ import os
 
 from tornadio2 import SocketConnection, TornadioRouter, SocketServer
 import tornado
-from tornado.web import RequestHandler, Application
+from tornado.web import RequestHandler, Application, StaticFileHandler
 
 from core import port
 
 from commands import RecordHandler, ResetPeaksHandler
 from status import Peaks, Status, Ping
 
-try:
-    class IndexHandler(RequestHandler):
-        def get(self):
-            self.render('index.html')
+class IndexHandler(RequestHandler):
+    def get(self):
+        self.render('index.html')
 
 
-    class RouterConnection(SocketConnection):
-        __endpoints__ = {'/peaks': Peaks,
-                         '/ping': Ping,
-                         '/status': Status}
+class RouterConnection(SocketConnection):
+    __endpoints__ = {'/peaks': Peaks,
+                     '/ping': Ping,
+                     '/status': Status}
 
+def main():
 
-    SockRouter = TornadioRouter(RouterConnection)
+    try:
+        SockRouter = TornadioRouter(RouterConnection)
 
-    application = Application(SockRouter.apply_routes([(r"/", IndexHandler),
-                                                         (r"/record", RecordHandler),
-                                                         (r"/resetPeaks", ResetPeaksHandler)]),
-                               socket_io_port = 8000,
-                               static_path = os.path.join(os.path.dirname(__file__), "static")
-                               )
+        application = Application(SockRouter.apply_routes([(r"/", IndexHandler),
+                                                             (r"/record", RecordHandler),
+                                                             (r"/resetPeaks", ResetPeaksHandler),
+                                                             (r"/flac/(.*)",tornado.web.StaticFileHandler, {"path": "."},)]),
+                                   socket_io_port = 80,
+                                   static_path = os.path.join(os.path.dirname(__file__), "static")
+                                   )
 
-    if __name__ == "__main__":
         socketio_server = SocketServer(application)
-finally:
-    port.stop()
-    port.waitTillFinished()
+    finally:
+        port.stop()
+        port.waitTillFinished()
 
 
+if __name__=="__main__":
+    main()
+    
