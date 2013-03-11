42b0efdcdca419a7a87e29cd511820cdaa40edc5
Andrew Loewenstern <drue@gigagig.org>
2013-03-10 19:15:57 -0700

    wifi / hostapd changes

--- chef/cookbooks/james/recipes/wifi.rb
+++ chef/cookbooks/james/recipes/wifi.rb
@@ -1,27 +1,14 @@
-package "bridge-utils"
-package "iptables"
-package "dhcp3-server"
 package "hostapd"
+package "dnsmasq"
 
 service "hostapd"
+service "dnsmasq"
 
 cookbook_file "/etc/network/interfaces" do
   mode "0644"
 end
 
-cookbook_file "/etc/sysctl.conf" do
-  mode "0644"
-end
-
-cookbook_file "/etc/iptables.rules" do
-  mode "0644"
-end
-
-cookbook_file "/etc/dhcp/dhcpd.conf" do
-  mode "0644"
-end
-
-cookbook_file "/etc/default/dhcp3-server" do
+cookbook_file "/etc/default/hostapd" do
   mode "0644"
 end
 
@@ -30,7 +17,8 @@ cookbook_file "/etc/hostapd/hostapd.conf" do
   notifies :restart, "service[hostapd]"
 end
 
-cookbook_file "/etc/default/hostapd" do
-  mode "0644"
-end
 
+cookbook_file "/etc/dnsmasq.conf" do
+  mode "0600"
+  notifies :restart, "service[dnsmasq]"
+end
