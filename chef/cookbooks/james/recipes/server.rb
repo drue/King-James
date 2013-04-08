5166beb1ee1170188744ca2d06e290d869d21ac1
Andrew Loewenstern <drue@gigagig.org>
2013-04-08 01:57:55 -0700

    do not wait at boot for nonexistent keyboard

--- chef/cookbooks/james/recipes/server.rb
+++ chef/cookbooks/james/recipes/server.rb
@@ -4,6 +4,16 @@ end
 
 cookbook_file "/etc/init/james.conf" do
   source "upstart.conf"
+  mode "0644"
+end
+
+execute "update-grub" do
+  action :nothing
+end
+
+cookbook_file "/etc/grub.d/00_header" do
+  mode "0755"
+  notifies :run, "execute[update-grub]", :immediately
 end
 
 service "james" do
