008674b48a8310478d921d7536e7399495bab92d
Andrew Loewenstern <drue@gigagig.org>
2014-04-20 10:53:01 -0700

    correct directory name

--- web/config.py
+++ web/config.py
@@ -4,4 +4,4 @@ config = ConfigParser()
 config.read("/etc/james/james.conf")
 
 def save_config():
-    config.write(open("/etc/qames/james.conf", "w"))
+    config.write(open("/etc/james/james.conf", "w"))
