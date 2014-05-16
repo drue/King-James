a4b57a6082d13019ad6e016e176f597fa160d70c
Andrew Loewenstern <drue@gigagig.org>
2014-05-15 17:34:40 -0700

    missing dummy transport

---
+++ web/devport.py
@@ -0,0 +1,56 @@
+from time import time
+import json
+from tornado.ioloop import IOLoop
+import zmq
+from zmq.eventloop.zmqstream import ZMQStream
+
+class DevPort(object):
+    def __init__(self, card, depth, rate):
+        self.card = card
+        self.depth = depth
+        self.rate = rate
+        self.running = False
+
+        context = zmq.Context() 
+
+        self.prog_socket = context.socket(zmq.PUB)
+        self.prog_socket.connect('ipc:///tmp/progressIn.ipc')
+        self.prog_stream = ZMQStream(self.prog_socket, IOLoop.instance())
+
+        self.scheduleProgress()
+        
+    def startRecording(self, path):
+        self.curPath = path
+        self.started = time()
+        self.running = True
+
+    def stopRecording(self):
+        self.running = False
+
+    def resetPeaks(self):
+        pass
+
+    def stop(self):
+        self.stopRecording()
+
+    def  waitTillFinished(self):
+        pass
+
+    def gotSignal(self):
+        return True
+
+    def sendProgress(self):
+        duration = 0
+        mode = 0
+        if self.running:
+            duration = int((time() - self.started) * self.rate)
+            mode = 1
+
+        d = {'t':duration, 'm': mode, 'b' : 0 }
+
+        s = json.dumps(d)
+        self.prog_stream.send(s)        
+        IOLoop.instance().add_timeout(time() + .333, self.sendProgress)
+        
+    def scheduleProgress(self):
+        IOLoop.instance().add_timeout(time() + .333, self.sendProgress)
