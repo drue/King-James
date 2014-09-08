2f94e7e305668683885eb185ef86929b6a4e4782
Andrew Loewenstern <drue@gigagig.org>
2014-09-07 19:45:01 -0700

    ditch proxy

--- chef/local/proxy/libraries/proxy.rb
+++
@@ -1,2 +0,0 @@
-ENV['HTTP_PROXY'] = 'http://10.0.2.2:3128/'
-ENV['http_proxy'] = 'http://10.0.2.2:3128/'
--- chef/local/proxy/recipes/default.rb
+++
@@ -1,6 +0,0 @@
- template "/etc/apt/apt.conf" do
-   source "apt.conf.erb"
-   variables(
-             :proxy => '"http://10.0.2.2:3128/"'
-             )
- end
